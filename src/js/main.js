b2b3454f24de99ce9d46e465b1de2fa56afb4985
jcaldia900 <j.calderon.diaz@iescristobaldemonroy.es>
2021-05-19 14:01:28 +0200

    modificaciones 19/05

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ import Swiper from "swiper/bundle";
 import Masonry from "masonry-layout";
 import imagesloaded from "imagesloaded";
 
-
 var swiper = new Swiper(".swiper-container", {
   pagination: {
     el: ".swiper-pagination",
@@ -68,10 +67,76 @@ if (document.getElementById("menuMobile") != null) {
   });
 }
 
+if (document.getElementById("botonDisponibilidad")) {
+  var boton = document.getElementById("botonDisponibilidad");
+  var resultado = boton.addEventListener("click", disponibilidad);
 
-  
+  function disponibilidad() {
+    if (document.getElementById("entrada")) {
+      var x = document.getElementById("entrada").value;
 
-if (document.getElementById("entrada")) {
+      let fechaEntrada = document.getElementById("entrada").value;
+      let fechaSalida = document.getElementById("salida").value;
+      let cantidad = document.getElementById("cantidad").value;
+      fechaEntrada = fechaEntrada.split("/").reverse().join("-");
+      fechaSalida = fechaSalida.split("/").reverse().join("-");
+      let entrada = new Date(fechaEntrada).getTime();
+
+      let salida = new Date(fechaSalida).getTime();
+      let diffTime = Math.abs(salida - entrada);
+      let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      document.getElementById("demo").innerHTML = diffDays;
+    }
+
+    
+    if (document.querySelector("input[name = regimen]:checked") && document.getElementById("entrada").value!=="" && document.getElementById("salida").value!=="" && document.getElementById("cantidad").value!=="") {
+      var regimen = document.querySelector("input[name = regimen]:checked").value;
+    
+    let params = {
+      hab:document.getElementById("habitacion").textContent,
+      fechaE: document.getElementById("entrada").value,
+      fechaS: document.getElementById("salida").value,
+      n_P: document.getElementById("cantidad").value,
+      reg: regimen,
+    };
+
+    let query = Object.keys(params)
+      .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(params[k]))
+      .join("&");
+
+    window.location.href = "http://localhost:8080/datospersonalesReserva?" + query;
+
+    fetch(window.location.href)
+      .then((data) => data.text())
+      .then((text) => {
+        console.log("request succeeded with JSON response", text);
+      })
+      .then((params) => {
+        console.log("Debe introducir los datos", params);
+      })
+      .catch(function (error) {
+        console.log("request failed", error);
+      });
+
+    } else {
+      alert("Introduzca todos los campos por favor");
+    }
+
+
+
+
+
+
+  }
+} 
+
+if (document.getElementById("fechaSalida")) {
+  document.getElementById("fechaSalida").innerHTML =
+    document.getElementById("salida").value;
+  console.log(document.getElementById("fechaSalida"));
+}
+
+/*if (document.getElementById("entrada")) {
   let fechaEntrada = document.getElementById("entrada").value;
   let fechaSalida = document.getElementById("salida").value;
   fechaEntrada = fechaEntrada.split("/").reverse().join("-");
@@ -88,18 +153,19 @@ if (document.getElementById("entrada")) {
 function disponibilidad(){
   var x = document.getElementById("entrada").value;
   document.getElementById("demo").innerHTML = x;
+}*/
+
+if (document.querySelector(".gridPanel")) {
+  var grid = document.querySelector(".gridPanel");
+
+  var msnry = new Masonry(grid, {
+    itemSelector: ".gridPanel-item",
+    columnWidth: ".gridPanel-sizer",
+    percentPosition: true,
+  });
+
+  imagesloaded(grid).on("progress", function () {
+    // layout Masonry after each image loads
+    msnry.layout();
+  });
 }
-
-if(document.querySelector('.gridPanel')){
-var grid = document.querySelector('.gridPanel');
-
-var msnry = new Masonry( grid, {
-  itemSelector: '.gridPanel-item',
-  columnWidth: '.gridPanel-sizer',
-  percentPosition: true
-});
-
-imagesloaded( grid ).on( 'progress', function() {
-  // layout Masonry after each image loads
-  msnry.layout();
-});}
