d06aa6af1c8a98133e37c1c2c1b7857b8e9c75ba
jcaldia900 <j.calderon.diaz@iescristobaldemonroy.es>
2021-05-26 12:28:34 +0200

    modificaciones 26/05

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import Swiper from "swiper/bundle";
 import Masonry from "masonry-layout";
 import imagesloaded from "imagesloaded";
+import { data } from "autoprefixer";
 
 var swiper = new Swiper(".swiper-container", {
   pagination: {
@@ -67,19 +68,7 @@ if (document.getElementById("menuMobile") != null) {
   });
 }
 
-/*if(document.getElementById("entrada").value!=="" && document.getElementById("salida").value!=="" ){
-  let fechaEntrada = document.getElementById("entrada").value;
-  let fechaSalida = document.getElementById("salida").value;
-  let cantidad = document.getElementById("cantidad").value;
-  fechaEntrada = fechaEntrada.split("/").reverse().join("-");
-  fechaSalida = fechaSalida.split("/").reverse().join("-");
-  let entrada = new Date(fechaEntrada).getTime();
- 
-  let salida = new Date(fechaSalida).getTime();
-  let diffTime = Math.abs(salida - entrada);
-  let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  document.getElementById("demo").innerHTML = diffDays;
-}*/
+
 
 if (document.getElementById("botonDisponibilidad")) {
   var boton = document.querySelectorAll(".botonDisponibilidad");
@@ -147,36 +136,7 @@ if (document.getElementById("botonDisponibilidad")) {
   });
 }
 
-var boton = document.querySelectorAll(".botonReservaAtras");
-boton.forEach((element) => {
-  var resultado = element.addEventListener("click", reservarAhoraAtras);
 
-  function reservarAhoraAtras() {
-    let params = {
-      fechaERes: document.getElementById("entradaReserva").value,
-      fechaSRes: document.getElementById("salidaReserva").value,
-      n_pers: document.getElementById("n_personas").value,
-    };
-
-    let query = Object.keys(params)
-      .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(params[k]))
-      .join("&");
-
-    window.location.href = "/reserva?" + query;
-
-    fetch(window.location.href)
-      .then((data) => data.text())
-      .then((text) => {
-        console.log("request succeeded with JSON response", text);
-      })
-      .then((params) => {
-        console.log("Debe introducir los datos", params);
-      })
-      .catch(function (error) {
-        console.log("request failed", error);
-      });
-  }
-});
 
 if (document.getElementById("botonReserva")) {
   var boton = document.querySelectorAll(".botonReserva");
@@ -185,17 +145,17 @@ if (document.getElementById("botonReserva")) {
 
     function reservarAhora() {
       if (
-        document.getElementById("entradaReserva").value !== "" &&
-        document.getElementById("salidaReserva").value !== ""
+        document.getElementById("entrada").value !== "" &&
+        document.getElementById("salida").value !== ""
       ) {
         if (
-          document.getElementById("salidaReserva").value >
-          document.getElementById("entradaReserva").value
+          document.getElementById("salida").value >
+          document.getElementById("entrada").value
         ) {
           let params = {
-            fechaERes: document.getElementById("entradaReserva").value,
-            fechaSRes: document.getElementById("salidaReserva").value,
-            n_pers: document.getElementById("n_personas").value,
+            fechaE: document.getElementById("entrada").value,
+            fechaS: document.getElementById("salida").value,
+            n_P: document.getElementById("n_P").value,
 
             // dif: document.getElementById("demo").value
           };
@@ -230,9 +190,14 @@ if (document.getElementById("botonReserva")) {
     }
   });
 }
+const queryStrings = window.location.search;
+const urlParametros = new URLSearchParams(queryStrings);
+const productos = urlParametros.get(urlParametros);
+const pt=urlParametros.get('fechaSRes');
+console.log(queryStrings);
 
-let fechaEntrada = getParameterByEntrada("fechaE");
-let fechaSalida = getParameterBySalida("fechaS");
+let fechaEntrada = urlParametros.get("fechaE");
+let fechaSalida = urlParametros.get("fechaS");
 
 fechaEntrada = fechaEntrada.split("/").reverse().join("-");
 fechaSalida = fechaSalida.split("/").reverse().join("-");
@@ -243,82 +208,39 @@ let salida = new Date(fechaSalida).getTime();
 let diffTime = Math.abs(salida - entrada);
 let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-function getParameterByEntrada(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var prodIdE = getParameterByEntrada("fechaE");
+
+var prodIdE = urlParametros.get("fechaE");
 var fechaE = document.getElementById("fechaEntrada");
 if (document.getElementById("fechaEntrada")) {
   fechaE.innerHTML = prodIdE;
 }
 
-function getParameterBySalida(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var prodIdS = getParameterBySalida("fechaS");
+
+var prodIdS = urlParametros.get("fechaS");
 var fechaS = document.getElementById("fechaSalida");
 if (document.getElementById("fechaSalida")) {
   fechaS.innerHTML = prodIdS;
 }
 
-function getParameterByHab(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var prodHab = getParameterByHab("hab");
+var prodHab = urlParametros.get("hab");
 
-function getParameterByNP(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var prodNP = getParameterByNP("n_P");
+var prodNP =  urlParametros.get("n_P");
 
-function getParameterByR(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var prodR = getParameterByR("reg");
+var prodR =  urlParametros.get("reg");
 
 var precio = document.getElementById("precioFinal");
 var precioFinal =
   parseInt(prodHab) * diffDays + parseInt(prodNP * prodR) * diffDays + "€";
+  console.log(precioFinal)
 if (document.getElementById("precioFinal")) {
   precio.innerHTML = precioFinal;
   document.getElementById("precioFinal").dataset.preciof = precioFinal;
 }
 
-function getParameterByTipo(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var tipoUrl = getParameterByTipo("tipoH");
+
+var tipoUrl = urlParametros.get("tipoH");
 var tipo = document.getElementById("textoHab");
+
 if (document.getElementById("textoHab")) {
   tipo.innerHTML = tipoUrl;
   document.getElementById("textoHab").dataset.hab = tipoUrl;
@@ -360,17 +282,13 @@ if (document.getElementById("botonFinalizar")) {
             tar: document.getElementById("numeroTarjeta").value,
             tit: document.getElementById("titularTarjeta").value,
             cad: document.getElementById("caducidadTarjeta").value,
-            precio: element.dataset.preciof,
-            f_E: getParameterByEntrada("fechaE"),
-            f_S: getParameterByEntrada("fechaS"),
-            tipoHCon: getParameterByEntrada("tipoH"),
-            img: getParameterByEntrada("img"),
-
-            precioHabCon: getParameterByEntrada("hab"),
-            numeroPConf: getParameterByEntrada("n_P"),
-            regimenCon: getParameterByEntrada("reg"),
-
-            // dif: document.getElementById("demo").value
+            precioFinal: document.getElementById("precioFinal").dataset.preciof,
+            fechaE: prodIdE,
+            fechaS: urlParametros.get("fechaS"),
+            tipoHCon: urlParametros.get("tipoH"),
+            img: urlParametros.get("img"),
+            
+           
           };
 
           let query = Object.keys(params)
@@ -402,209 +320,84 @@ if (document.getElementById("botonFinalizar")) {
   });
 }
 
-function getParameterByNombreConfirmacion(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parNomCon = getParameterByNombreConfirmacion("nom");
+
+var parNomCon = urlParametros.get("nom");
 var nombreC = document.getElementById("nombreConfirmacion");
 if (document.getElementById("nombreConfirmacion")) {
   nombreC.innerHTML = parNomCon;
 }
 
-function getParameterByApellidosConfirmacion(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parApellCon = getParameterByApellidosConfirmacion("apel");
+var parApellCon = urlParametros.get("apel");
 var apellC = document.getElementById("apellidosConfirmacion");
 if (document.getElementById("apellidosConfirmacion")) {
   apellC.innerHTML = parApellCon;
 }
 
-function getParameterByEmailConfirmacion(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parEmailCon = getParameterByEmailConfirmacion("mail");
+
+var parEmailCon = urlParametros.get("mail");
 var emailC = document.getElementById("emailConfirmacion");
 if (document.getElementById("emailConfirmacion")) {
   emailC.innerHTML = parEmailCon;
 }
 
-function getParameterByTelefonoConfirmacion(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parTfnoCon = getParameterByTelefonoConfirmacion("tfno");
+
+var parTfnoCon = urlParametros.get("tfno");
 var tfnoC = document.getElementById("telefonoConfirmacion");
 if (document.getElementById("telefonoConfirmacion")) {
   tfnoC.innerHTML = parTfnoCon;
 }
 
-function getParameterByNumTarjetaConfirmacion(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parTarCon = getParameterByNumTarjetaConfirmacion("tar");
+var parTarCon = urlParametros.get("tar");
 var tarjetaC = document.getElementById("num_tarjetaConfirmacion");
 if (document.getElementById("num_tarjetaConfirmacion")) {
   tarjetaC.innerHTML = parTarCon;
 }
 
-function getParameterByNombreTitular(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parNomTitCon = getParameterByNombreTitular("tit");
+
+var parNomTitCon = urlParametros.get("tit");
 var nombreTitC = document.getElementById("nombreTitularConfirmacion");
 if (document.getElementById("nombreTitularConfirmacion")) {
   nombreTitC.innerHTML = parNomTitCon;
 }
 
-function getParameterByCaducidadConfirmacion(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parCadCon = getParameterByCaducidadConfirmacion("cad");
+
+var parCadCon = urlParametros.get("cad");
 var caducidadC = document.getElementById("caducidadConfirmacion");
 if (document.getElementById("caducidadConfirmacion")) {
   caducidadC.innerHTML = parCadCon;
 }
 
-function getParameterByF_SConfirmacion(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parSalidaCon = getParameterByF_SConfirmacion("f_S");
+
+var parSalidaCon = urlParametros.get("fechaS");
 var salidaCon = document.getElementById("fechaSalidaConfirmacion");
 if (document.getElementById("fechaSalidaConfirmacion")) {
   salidaCon.innerHTML = parSalidaCon;
 }
 
-function getParameterByF_EConfirmacion(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parEntradaCon = getParameterByF_EConfirmacion("f_E");
+
+var parEntradaCon = urlParametros.get("fechaE");
 var entradaCon = document.getElementById("fechaEntradaConfirmacion");
 if (document.getElementById("fechaEntradaConfirmacion")) {
   entradaCon.innerHTML = parEntradaCon;
 }
 
-function getParameterByHabConfirmacion(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var parHabCon = getParameterByHabConfirmacion("tipoHCon");
+
+var parHabCon = urlParametros.get("tipoHCon");
 var habCon = document.getElementById("textoHabConfirmacion");
 if (document.getElementById("textoHabConfirmacion")) {
   habCon.innerHTML = parHabCon;
 }
-/*precioHabCon:getParameterByEntrada("hab"),
-numeroPConf:getParameterByEntrada("n_P"),
-regimenCon:getParameterByEntrada("reg")*/
 
-function getParameterNPCon(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var prodNPConfirmacion = getParameterNPCon("numeroPConf");
 
-function getParameterPrecioHabConf(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var prodHabConfirmacion = getParameterPrecioHabConf("precioHabCon");
-
-function getParameterRegimenCon(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-var prodRConfirmacion = getParameterRegimenCon("regimenCon");
-
-let fechaEntradaConfi = getParameterByEntrada("f_E");
-let fechaSalidaConfi = getParameterBySalida("f_S");
-
-fechaEntradaConfi = fechaEntradaConfi.split("/").reverse().join("-");
-fechaSalidaConfi = fechaSalidaConfi.split("/").reverse().join("-");
-let entradaConfi = new Date(fechaEntradaConfi).getTime();
-
-let salidaConfi = new Date(fechaSalidaConfi).getTime();
-
-let diffTimeConfi = Math.abs(salidaConfi - entradaConfi);
-let diffDaysConfi = Math.ceil(diffTimeConfi / (1000 * 60 * 60 * 24));
 
 var precioConf = document.getElementById("precioFinalConfirmacion");
-var precioFinalConf =
-  parseInt(prodHabConfirmacion) * diffDaysConfi +
-  parseInt(prodNPConfirmacion * prodRConfirmacion) * diffDaysConfi +
-  "€";
+
 if (document.getElementById("precioFinalConfirmacion")) {
-  precioConf.innerHTML = precioFinalConf;
+  precioConf.innerHTML = urlParametros.get("precioFinal");
 }
-function getParameterByImg(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+
 if (document.getElementById("imgConfirmacion")) {
-  var parImgCon = getParameterByImg("img");
+  var parImgCon = urlParametros.get("img");
   var imgCon = document
     .getElementById("imgConfirmacion")
     .setAttribute("src", parImgCon);
@@ -625,17 +418,26 @@ var fechaEntradaConfirmacionJSON=document.getElementById("fechaEntradaConfirmaci
 var fechaSalidaConfirmacionJSON=document.getElementById("fechaSalidaConfirmacion");
 var imgConfirmacionJSON=document.getElementById("imgConfirmacion");
 var textoHabConfirmacionJSON=document.getElementById("textoHabConfirmacion");
+var precioConfirmacionJSON=document.getElementById("precioFinalConfirmacion");
 
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const entradaRes = urlParams.get("fechaERes");
-const salidaRes = urlParams.get("fechaSRes");
-const numeroRes = urlParams.get("n_pers");
+const entradaRes = urlParams.get("fechaE");
+const salidaRes = urlParams.get("fechaS");
+
 
 const nombreCJSON=urlParams.get("nom");
 const apellidosCJSON=urlParams.get("apel");
+const mailResJSON = urlParams.get("mail");
+const tfnoResJSON = urlParams.get("tfno");
+const tarResJSON = urlParams.get("tar");
+const nomTitResJSON = urlParams.get("tit");
+const cadResJSON = urlParams.get("cad");
+const imgResJSON = urlParams.get("img");
+const tipoHJSON = urlParams.get("tipoHCon");
+const precioJSON = urlParams.get("precioFinal");
 
 
 const datos_reserva = {
@@ -648,10 +450,18 @@ const datos_reserva = {
 const datosConfirmacion={
   nombreConfirmacionJSON:nombreCJSON,
   apellidosConfirmacionJSON:apellidosCJSON,
-  precioFinalConf
-
+  emailConfirmacionJSON:mailResJSON,
+  telefonoConfirmacionJSON:tfnoResJSON,
+  num_tarjetaConfirmacionJSON:tarResJSON,
+  nombreTitularConfirmacionJSON:nomTitResJSON,
+  caducidadConfirmacionJSON:cadResJSON,
+  fechaEntradaConfirmacionJSON:entradaRes,
+  fechaSalidaConfirmacionJSON:salidaRes,
+  imgConfirmacionJSON:imgResJSON,
+  textoHabConfirmacionJSON:tipoHJSON,
+  precioConfirmacionJSON:precioJSON
 }
-console.log(datos_reserva);
+
 
 if(entradaParameter){
 entradaParameter.value = datos_reserva.entradaParameter;}
